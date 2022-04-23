9b560dea89efbc359cef65fa60f63bac84adcb6c
Gleb Batykov <gleb.batykov@gmail.com>
2022-04-23 20:07:07 +0300

    Create rephares result page cubit

---
+++ lib/presentation/cubit/page/history/result/rephrase/rephrase_result_page_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import '../../../../../model/page/history/rephrase_result_log.dart';
+
+part 'rephrase_result_page_state.dart';
+
+class RephraseResultPageCubit extends Cubit<RephraseResultPageState> {
+  RephraseResultPageCubit(RephraseResultInfo info)
+      : super(RephraseResultPageInitial());
+}
