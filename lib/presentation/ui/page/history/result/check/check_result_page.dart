039e700f6d826c518d4320dc17b42bb5abe5d166
Gleb Batykov <gleb.batykov@gmail.com>
2022-04-26 00:47:58 +0300

    Renames check and rephrase result page widgets

---
+++ lib/presentation/ui/page/history/result/check/check_result_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+import '../../../../../cubit/page/history/result/check/check_result_page_cubit.dart';
+import 'body/check_result_page_body.dart';
+import 'title_bar/check_result_page_title_bar.dart';
+
+class CheckResultPage extends StatelessWidget {
+  const CheckResultPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CheckResultPageCubit, CheckResultPageState>(
+        builder: (context, state) {
+      if (state is CheckResultPageShow) {
+        return Scaffold(
+          backgroundColor: HexColor('#301B28'),
+          body: Column(
+            children: [
+              CheckResultPageTitleBar(percentages: state.percentages),
+              const Expanded(child: CheckResultPageBody())
+            ],
+          ),
+        );
+      } else {
+        return Scaffold(
+          backgroundColor: HexColor('#301B28'),
+        );
+      }
+    });
+  }
+}
--- lib/presentation/ui/page/history/result/rephrase/rephrase_result_page.dart
+++ lib/presentation/ui/page/history/result/rephrase/rephrase_result_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:synword_flutter_cubit_application/presentation/ui/page/history/result/check/body/check_result_page_body.dart';
+import 'package:synword_flutter_cubit_application/presentation/ui/page/history/result/check/title_bar/check_result_page_title_bar.dart';
 
 class RephraseResultPage extends StatelessWidget {
   const RephraseResultPage({Key? key}) : super(key: key);
