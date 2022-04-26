2e4e8eef10711e4651c0e7bb789fd092cc4a8d1b
Batykov Gleb <gleb.batykov@gmail.com>
2022-04-26 16:38:53 +0300

    Adds assets

--- lib/presentation/asset/history_page_asset.dart
+++ lib/presentation/asset/history_page_asset.dart
@@ -1,3 +1,9 @@
 abstract class HistoryPageAsset {
   static const clock = 'assets/images/history-page/clock.png';
+
+  static const note = 'assets/images/history-page/note.png';
+
+  static const website = 'assets/images/history-page/website.png';
+
+  static const show = 'assets/images/history-page/show.png';
 }
