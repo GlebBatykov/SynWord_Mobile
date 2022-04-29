6eba9bb427e95ad2d4185984763b3350db220146
Gleb Batykov <gleb.batykov@gmail.com>
2022-04-29 14:07:53 +0300

    Creates copy and download file button widgets

---
+++ lib/presentation/ui/button/rounded/rounded_download_file_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../../../asset/history_page_asset.dart';
+import 'rounded_button.dart';
+
+class RoundedDownloadFileButton extends StatelessWidget {
+  const RoundedDownloadFileButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RoundedButton(
+      onTap: () {},
+      icon: Padding(
+          padding: const EdgeInsets.only(left: 8, top: 8, right: 7, bottom: 7),
+          child: Image.asset(HistoryPageAsset.downloadFile)),
+    );
+  }
+}
