5a985ae14ca4a7ea1c464a0cb75a292f2f51f6de
Gleb Batykov <gleb.batykov@gmail.com>
2022-06-21 14:57:33 +0300

    Updates main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'data/mapper/mapper_box_configuration.dart';
 import 'injaction_contaner.dart';
 import 'presentation/navigation/navigation_builder.dart';
 import 'presentation/ui/application.dart';
 import 'system_chrome_configuration.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await InjactionContainer().initialize();
 
-  NavigationBuilder().build();
+  MapperBoxConfiguration().configurate();
 
-  WidgetsFlutterBinding.ensureInitialized();
+  NavigationBuilder().build();
 
   await SystemChromeConfiguration().configure();
 
