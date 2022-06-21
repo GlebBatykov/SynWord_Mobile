7f7b3074adedf54da8edea9811fff46f471576bb
Gleb Batykov <gleb.batykov@gmail.com>
2022-06-21 14:56:48 +0300

    Adds get token use case

---
+++ lib/domain/use_case/get_token_use_case.dart
@@ -0,0 +1,51 @@
+import 'package:get_it/get_it.dart';
+
+import '../model/token.dart';
+import '../model/user.dart';
+import '../repository/local/user_local_repository.dart';
+import '../repository/remote/user_remote_repository.dart';
+
+class GetTokenUseCase {
+  final UserLocalRepository _userLocalRepository =
+      GetIt.instance<UserLocalRepository>();
+
+  final UserRemoteRepository _userRemoteRepository =
+      GetIt.instance<UserRemoteRepository>();
+
+  late User _user;
+
+  Future<Token> getToken() async {
+    _user = _userLocalRepository.getUser();
+
+    if (_user.token == null || (_user.token != null && !_user.token!.isValid)) {
+      var token = await _getTokenFromRemote();
+
+      _user.token = token;
+
+      await _userLocalRepository.save();
+    }
+
+    return _user.token!;
+  }
+
+  Future<Token> _getTokenFromRemote() async {
+    late Token token;
+
+    if (_user.accessToken != null) {
+      token =
+          await _userRemoteRepository.getTokenByGoogleToken(_user.accessToken!);
+    } else {
+      if (_user.id == null) {
+        var id = await _userRemoteRepository.registerUser();
+
+        _user.id = id;
+
+        await _userLocalRepository.save();
+      }
+
+      token = await _userRemoteRepository.getTokenByUserId(_user.id!);
+    }
+
+    return token;
+  }
+}
